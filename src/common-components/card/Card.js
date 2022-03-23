78f44669dc9e36252825e171200713f5f2f12b54
danilo-viveiros <60623256+danilo-viveiros@users.noreply.github.com>
2022-03-23 19:52:18 -0300

    Configurar os components restantes

---
+++ src/common-components/card/Card.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import { BorderRadiuses, Colors } from '../../shared/DesignTokens'
+export const Card = styled.div`
+    width:100%;
+    height: 100%;
+    border: 1px solid ${Colors.GRAY_200};
+    border-radius: ${BorderRadiuses.ONE};
+    `;
+    
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,13 +1,29 @@
 import { Button } from '../common-components/button/Button';
+import { Caption } from '../common-components/caption/Caption';
+import { Card } from '../common-components/card/Card';
+import { Description } from '../common-components/description/Description';
 import { Header } from '../common-components/header/Header';
+import { HeadingOne } from '../common-components/headingOne/HeadingOne';
+import { HeadingTwo } from '../common-components/headingTwo/HeadingTwo';
 import { SearchField } from '../common-components/searchField/SearchField';
+import { Subtitle } from '../common-components/subtitle/Subtitle';
 export function Search() {
 	return (
 		<div>
 			<Header />
 			<SearchField placeholder="Digite um nome de herói ou heroína" />
 			<Button ghost={true}>Buscar</Button>
+			<Card>
+					<HeadingOne>Heading One</HeadingOne>
+					<HeadingTwo>Heading Two</HeadingTwo>
+				<div>
+					<Subtitle>Subtitle</Subtitle>
+				</div>
+					<Description>Description</Description>
+				<div>
+					<Caption>Caption</Caption>
+				</div>
+			</Card>
 		</div>
 	);
 }
-
