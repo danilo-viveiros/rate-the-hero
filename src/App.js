46c39368f15546b600b7b1c51864aee8830e15c2
Danilo Viveiros <danilovive@hotmail.com>
2022-03-24 16:47:19 -0300

    Pronto para proxima fase

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Routes, Route } from "react-router-dom";
 import { Details } from "./screens/Details";
 import { NotFound } from "./screens/NotFound";
 import { Search } from "./screens/Search";
 import { NormalizeStyles } from "./shared/NormalizeStyles"
-
+import { Header } from "./common-components/header/Header"
 function App() {
   return (
 
   <>
   <NormalizeStyles/>
     <BrowserRouter>
+    <Header/>
      <Routes>
       <Route path="/detalhes/:id" element={<Details/>}/>
       <Route path="/" element={<Search/>}/>
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -16,31 +16,7 @@ import { Subtitle } from '../common-components/subtitle/Subtitle';
 export function Search() {
 	return (
 		<div>
-			<Header />
-			<SearchField placeholder="Digite um nome de herói ou heroína" />
-			<Button ghost={true}>Buscar</Button>
-			<Card>
-				<HeadingOne>Heading One</HeadingOne>
-				<HeadingTwo>Heading Two</HeadingTwo>
-				<div>
-					<Subtitle>Subtitle</Subtitle>
-				</div>
-				<Description>Description</Description>
-				<div>
-					<Caption>Caption</Caption>
-				</div>
-			</Card>
-			<ButtonLink>Ver Mais</ButtonLink>
-			<SelectField placeholder="Nota">
-				<Option>Opção 1</Option>
-			</SelectField>
-			<Alert type="info">Nenhum herói ou heroína encontrado</Alert>
-			<Alert type="success" small={true}>
-				Sua avaliação foi atribuída com sucesso!
-			</Alert>
-			<Alert type="error" small={true}>
-				Ocorreu um erro ao avaliar
-			</Alert>
+			
 		</div>
 	);
 }
